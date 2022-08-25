dd62a2c9b2a095e6ee12d9b20b040a791d787ba8
Zaid <shaikhzakariya837@gmail.com>
2022-08-25 09:41:17 +0530

    First Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn legends.wsgi --log-file -
---
+++ store/admin.py
@@ -0,0 +1,12 @@
+from collections import _OrderedDictItemsView
+from django.contrib import admin
+
+# Register your models here.
+
+from . models import *
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItems)
+admin.site.register(ShippingAddress)
